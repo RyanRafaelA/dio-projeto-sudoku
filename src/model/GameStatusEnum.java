e7cd6249bf00998c4fada06dc6f721cf9e6199d6
RyanRafaelA <ryrafael461@gmail.com>
2025-02-25 22:20:28 -0300

    Jogo finalizado

--- src/model/GameStatusEnum.java
+++ src/model/GameStatusEnum.java
@@ -2,7 +2,17 @@ package model;
 
 public enum GameStatusEnum {
 
-    NOW_STARTED,
-    INCOMPLETE,
-    COMPLETE
+    NOW_STARTED("não iniciado"),
+    INCOMPLETE("Incompleto"),
+    COMPLETE("completo");
+
+    private String label;
+
+    GameStatusEnum(final String label){
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
